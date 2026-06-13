76671a0d265e75823eaf6032389e2b30c00dc6ea
aryansoni210209-netizen <aryansoni210209@gmail.com>
2026-06-13 16:20:12 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+using namespace std;
+
+int main() { 
+
+    int n;
+    cin >> n;
+    int num = 1;
+    int i = 1; 
+    while(i <= n ) { 
+        int j = 1 ;
+        while(j <= n ) { 
+           
+            cout << num <<" ";
+            num = num + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+
+    
+    }
+}
