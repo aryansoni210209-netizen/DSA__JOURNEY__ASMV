74013927356df68c9bad5533c9ed6bb82884ac52
aryansoni210209-netizen <aryansoni210209@gmail.com>
2026-06-13 16:19:27 +0530

    Create bintodec.cpp

---
+++ bintodec.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int i = 0;
+    int ans = 0;
+    while(n>0){
+        int num = n % 10;
+        if(num == 1){
+            ans = pow(2,i) + ans;
+        }
+        n = n / 10;
+        i++;
+    }
+    cout << ans << endl;
+}
