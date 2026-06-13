d9cee6bb8ae206bc05b095cc4e62ab7818484a43
aryansoni210209-netizen <aryansoni210209@gmail.com>
2026-06-13 16:19:24 +0530

    Create aryan.cpp

---
+++ aryan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    cout << "enter first number: ";
+    int a;
+    cin >> a;
+    cout << "enter second number: ";
+    int b;
+    cin >> b;
+    cout << "the sum of the two numbers is: " << a + b;
+     
+    return 0;
+}
